c03b9534da11f794c30511b3290823afff9bfe62
mpividori <pividori@esteco.com>
2024-02-26 18:47:09 +0100

    fix

--- apps/streaming-companion-web/src/app/spreadsheet/spreadsheet.ts
+++ apps/streaming-companion-web/src/app/spreadsheet/spreadsheet.ts
@@ -1,10 +1,9 @@
 import {GoogleSpreadsheet} from 'google-spreadsheet';
-import {AuthUtil} from '../auth/auth.util';
 
 export class Spreadsheet {
 
   public static async openSpreadsheet(spreadsheetId: string): Promise<GoogleSpreadsheet> {
-    const serviceAccountAuth = AuthUtil.createServiceAccount();
+    const serviceAccountAuth = {} as any;
     const spreadsheet = new GoogleSpreadsheet(spreadsheetId, serviceAccountAuth);
     await spreadsheet.loadInfo();
     return spreadsheet;
