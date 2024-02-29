a8b8e80749c07baf86f26aaf7d334e211c5d8324
mpividori <pividori@esteco.com>
2024-02-29 23:30:32 +0100

    use new sheet for tournament

--- apps/streaming-companion-be/src/schemas.ts
+++ apps/streaming-companion-be/src/schemas.ts
@@ -16,17 +16,21 @@ export interface PoolRanks {
 }
 
 export const schemas: Schemas = {
-  spreadsheetId: '1WSziZtu7A-dnMk8gLMVhg3k3PGB9aZTsdE2vJLe6rIc',
+  spreadsheetId: '1XasRB15O4qkcv2V6IRfKiU5EobG263kJNKGrtDsNXDs',
   pools: [
     {name: 'poolA1', columnIndex: 1},
     {name: 'poolB1', columnIndex: 10},
     {name: 'poolA2', columnIndex: 19},
-    {name: 'poolB2', columnIndex: 28}
+    {name: 'poolB2', columnIndex: 28},
+    {name: 'poolA3', columnIndex: 37},
+    {name: 'poolB3', columnIndex: 46}
   ],
   poolRanks: [
     {name: 'poolA1', columnIndex: 1, rowIndex: 10},
     {name: 'poolB1', columnIndex: 10, rowIndex: 10},
     {name: 'poolA2', columnIndex: 19, rowIndex: 10},
-    {name: 'poolB2', columnIndex: 28, rowIndex: 10}
+    {name: 'poolB2', columnIndex: 28, rowIndex: 10},
+    {name: 'poolA3', columnIndex: 37, rowIndex: 10},
+    {name: 'poolB3', columnIndex: 46, rowIndex: 10}
   ]
 }
