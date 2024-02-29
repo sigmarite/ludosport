b89533510bd2d650030cacf604533bc062d1af43
mpividori <pividori@esteco.com>
2024-02-29 17:31:49 +0100

    update schemas

--- apps/streaming-companion/src/schemas.ts
+++ apps/streaming-companion/src/schemas.ts
@@ -18,15 +18,15 @@ export interface PoolRanks {
 export const schemas: Schemas = {
   spreadsheetId: '1WSziZtu7A-dnMk8gLMVhg3k3PGB9aZTsdE2vJLe6rIc',
   pools: [
-    {filename: 'apps/streaming-companion/outputs/poolA1.json', columnIndex: 1},
-    {filename: 'apps/streaming-companion/outputs/poolB1.json', columnIndex: 10},
-    {filename: 'apps/streaming-companion/outputs/poolA2.json', columnIndex: 19},
-    {filename: 'apps/streaming-companion/outputs/poolB2.json', columnIndex: 28}
+    {filename: '../streaming-companion-render/build/json/poolA1.json', columnIndex: 1},
+    {filename: '../streaming-companion-render/build/json/poolB1.json', columnIndex: 10},
+    {filename: '../streaming-companion-render/build/json/poolA2.json', columnIndex: 19},
+    {filename: '../streaming-companion-render/build/json/poolB2.json', columnIndex: 28}
   ],
   poolRanks: [
-    {filename: 'apps/streaming-companion/outputs/poolA1-ranks.json', columnIndex: 1, rowIndex: 10},
-    {filename: 'apps/streaming-companion/outputs/poolB1-ranks.json', columnIndex: 10, rowIndex: 10},
-    {filename: 'apps/streaming-companion/outputs/poolA2-ranks.json', columnIndex: 19, rowIndex: 10},
-    {filename: 'apps/streaming-companion/outputs/poolB2-ranks.json', columnIndex: 28, rowIndex: 10}
+    {filename: '../streaming-companion-render/build/json/poolA1-ranks.json', columnIndex: 1, rowIndex: 10},
+    {filename: '../streaming-companion-render/build/json/poolB1-ranks.json', columnIndex: 10, rowIndex: 10},
+    {filename: '../streaming-companion-render/build/json/poolA2-ranks.json', columnIndex: 19, rowIndex: 10},
+    {filename: '../streaming-companion-render/build/json/poolB2-ranks.json', columnIndex: 28, rowIndex: 10}
   ]
 }
