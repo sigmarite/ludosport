f036d05b222e669c688378b513d21372aaa6106b
Marco Pividori <pividori@esteco.com>
2024-10-24 18:59:48 +0200

    change port

--- apps/streaming-companion-be/src/main.ts
+++ apps/streaming-companion-be/src/main.ts
@@ -3,7 +3,7 @@ import {IncomSpreadsheet} from './spreadsheet/incom.spreadsheet';
 import {schemas} from './schemas';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3001;
 
 const app = express();
 
